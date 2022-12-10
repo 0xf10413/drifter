c4cba943d9a0f75f76f2ff160ca370a3f045185d
0xf10413 <florent.gattoni@ec14.ec-lyon.fr>
2022-12-11 00:37:55 +0100

    Fix model

--- webui/admin.py
+++ webui/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MachType, Phase, Server
+
+admin.site.register(MachType)
+admin.site.register(Phase)
+admin.site.register(Server)
--- webui/models.py
+++ webui/models.py
@@ -3,9 +3,19 @@ from django.db import models
 class MachType(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Phase(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Server(models.Model):
     mach_type = models.ForeignKey(MachType, on_delete=models.PROTECT)
     phase = models.ForeignKey(Phase, on_delete=models.PROTECT)
+    name = models.CharField(max_length=256, null=False)
+
+    def __str__(self) -> str:
+        return self.name
