d366e551e0fcd695df88307a3bd45fe4bb4733c2
0xf10413 <florent.gattoni@ec14.ec-lyon.fr>
2022-12-11 18:13:02 +0100

    Improve query in DB

--- webui/views.py
+++ webui/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from . import models, forms
+from django.db.models import Min
+from django.db.models.expressions import F
+from django.db.models.functions import Concat
+from django.db.models import Value as V
 import pydantic
 import logging
 import datetime as dt
 from collections import OrderedDict, defaultdict
 import statistics
+
 
 def index(request):
     context = {
@@ -13,69 +18,65 @@ def index(request):
     }
 
     # Gather data points for each date, grouped by (mach_type, phase).
-    raw_values_per_date = OrderedDict()
-    for stat in models.NbChanges.objects.order_by("-datetime").all():
-        if stat.datetime not in raw_values_per_date:
-            raw_values_per_date[stat.datetime] = defaultdict(list)
-        raw_values_per_date[stat.datetime][stat.server.mach_type.name,stat.server.phase.name].append(stat.nb_change)
-
-    # Convert raw values into max, min, ...
-    values_per_date = {}
-    groups = []
-    for datetime, grouped_stats in raw_values_per_date.items():
-        if datetime not in values_per_date:
-            values_per_date[datetime] = {}
-        for stat_group_name, stat_group in grouped_stats.items():
-            if stat_group_name not in groups:
-                groups.append(stat_group_name)
-            values_per_date[datetime][stat_group_name] = {
-                "min": min(stat_group),
-                "max": max(stat_group),
-                "mean": statistics.mean(stat_group)
-            }
+    queryset = (
+        models.NbChanges.objects.values(
+            "datetime", "server__phase__name", "server__mach_type__name"
+        )
+        .annotate(
+            min=Min("nb_change"),
+            phase=F("server__phase__name"),
+            mach_type=F("server__mach_type__name"),
+            group_name=Concat("mach_type", V(" - "), "phase"),
+        )
+        .order_by("-datetime")
+    )
 
     # Need to format data as a list of objets:
-    # { datetime: datetime1, server1: value1, server2: value2, ...}
-    datapoints = []
-    for datetime, values in values_per_date.items():
-        datapoints.append(
-            {
-                "datetime": datetime,
-                **values,
-            }
-        )
-    context["datapoints"] = datapoints
-    context["groups"] = [f"{group[0]} - {group[1]}" for group in groups]
-    return render(request, "webui/index.html.j2", context)
+    # { datetime: datetime1, series1: value1, series2: value2, ...}
+    datapoints = defaultdict(dict)
+    for result in queryset:
+        datapoints[result["datetime"]][result["group_name"] + "_min"] = result["min"]
+    context["datapoints"] = dict(datapoints)
+    context["group_names"] = {result["group_name"] for result in queryset}
+    return render(request, "webui/index.html", context)
+
 
 class PlayDuration(pydantic.BaseModel):
     start: dt.datetime
+
 
 class PlayMetadata(pydantic.BaseModel):
     name: str
     duration: PlayDuration
 
+
 class TaskHost(pydantic.BaseModel):
     changed: bool
 
+
 class TaskData(pydantic.BaseModel):
     name: str
+
 
 class Task(pydantic.BaseModel):
     hosts: dict[str, TaskHost]
     task: TaskData
 
+
 class Play(pydantic.BaseModel):
     play: PlayMetadata
     tasks: list[Task]
+
 
 class CustomStat(pydantic.BaseModel):
     mach_type: str
     phase: str
 
+
 class PlaybookOutput(pydantic.BaseModel):
     plays: list[Play]
     custom_stats: dict[str, CustomStat]
+
 
 def upload(request):
     if request.method == "POST":
@@ -84,33 +85,42 @@ def upload(request):
             context = {
                 "form": form,
             }
-            return render(request, "webui/upload.html.j2", context)
-    
-        playbook_output = PlaybookOutput.parse_raw(request.FILES['file_name'].read())
+            return render(request, "webui/upload.html", context)
+
+        playbook_output = PlaybookOutput.parse_raw(request.FILES["file_name"].read())
         logging.critical("Got playbook: %s", playbook_output)
         handle_new_ansible_input(playbook_output)
-
 
     # Prepare a new upload
     form = forms.UploadForm()
     context = {
         "form": form,
     }
-    return render(request, "webui/upload.html.j2", context)
+    return render(request, "webui/upload.html", context)
+
 
 def handle_new_ansible_input(output: PlaybookOutput) -> None:
     # Identify the machines, using custom stats
     stats_per_host: dict[str, models.NbChanges] = {}
     play_start = output.plays[0].play.duration.start
     for hostname, stat in output.custom_stats.items():
-        logging.warning("Detected host %s, which is a %s in phase %s", hostname, stat.mach_type, stat.phase)
+        logging.warning(
+            "Detected host %s, which is a %s in phase %s",
+            hostname,
+            stat.mach_type,
+            stat.phase,
+        )
         # Update machine configuration
         mach_type, _ = models.MachType.objects.update_or_create(name=stat.mach_type)
         phase, _ = models.Phase.objects.update_or_create(name=stat.phase)
-        host, _ = models.Server.objects.update_or_create(mach_type=mach_type, phase=phase, name=hostname)
-        
+        host, _ = models.Server.objects.update_or_create(
+            mach_type=mach_type, phase=phase, name=hostname
+        )
+
         # Retrieve change statistic corresponding to this playbook, if any, and reinitialize it
-        nb_changes, _ = models.NbChanges.objects.get_or_create(server=host, datetime=play_start)
+        nb_changes, _ = models.NbChanges.objects.get_or_create(
+            server=host, datetime=play_start
+        )
         nb_changes.nb_change = 0
         stats_per_host[hostname] = nb_changes
 
@@ -124,4 +134,3 @@ def handle_new_ansible_input(output: PlaybookOutput) -> None:
     # Save everything
     for stat in stats_per_host.values():
         stat.save()
-
